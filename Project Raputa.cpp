e85542e802f8830b55a031739e18f859e3c0fdba
Rafael Ira R. Villanueva <151484858+ChildishLambanog@users.noreply.github.com>
2024-01-14 16:51:57 +0800

    Add files via upload
    Assignment 1 Perfect Pentagon

---
+++ Project Raputa.cpp
@@ -0,0 +1,45 @@
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(800, 800, "Rafael Ira Rambano Villanueva", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_POLYGON);
+        glVertex2f(0.2167, 0.6656); // top
+        glVertex2f(0.7000, 0.0000); // right
+        glVertex2f(0.2146, -0.6663); // bottom right
+        glVertex2f(-0.5671, -0.4104); // bottom left
+        glVertex2f(-0.5658, 0.4122); // left 
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
